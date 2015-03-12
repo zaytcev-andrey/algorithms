a15fb89c0f44acf7a38ef5f91d47a7b1f4e5d4b3
Zaytsev.Andrey <Andrey.Zaytsev@infotecs.ru>
2015-03-12 18:04:55 +0300

    attempt to impl merge

--- skyline/include/skyline.h
+++ skyline/include/skyline.h
@@ -4,6 +4,7 @@
 #include <skyline/include/triple_building.h>
 
 #include <vector>
+#include <algorithm>
 #include <assert.h>
 #include <utils/include/utils.h>
 
@@ -15,7 +16,142 @@ namespace skyline
                , const std::vector< int >& right_skyline
                , std::vector< int >& res_skyline )
           {
+               const size_t left_len = left_skyline.size();
+               const size_t right_len = right_skyline.size();
+               
+               size_t left_idx = 0;
+               size_t right_idx = 0;
 
+               int current_height_left = 0;
+               int current_height_right = 0;
+
+               int current_left = left_skyline[ left_idx ];
+               int current_right = right_skyline[ right_idx ];
+
+               while( left_idx < left_len || right_idx < right_len )
+               {
+                    if ( current_left < current_right && left_idx < left_len )
+                    {    
+                         const int prev_height_left = current_height_left;
+
+                         current_height_left = left_skyline[ left_idx + 1 ];                         
+                         
+                         if ( current_height_left > current_height_right )
+                         {
+                              res_skyline.push_back( left_skyline[ left_idx ] );                                                  
+                              res_skyline.push_back( current_height_left );
+
+                         }
+
+                         if ( prev_height_left > current_height_right &&
+                              current_height_right != 0 && current_height_left == 0 )
+                         {
+                              res_skyline.push_back( left_skyline[ left_idx ] );                                                  
+                              res_skyline.push_back( current_height_right );
+                         }
+
+                         if ( current_height_right == 0 && current_height_left == 0 )
+                         {
+                              res_skyline.push_back( left_skyline[ left_idx ] );                                                  
+                              res_skyline.push_back( 0 );
+                         }
+
+                         left_idx += 2;
+                         if ( left_idx < left_len )
+                         {
+                              current_left = left_skyline[ left_idx ];
+                         }
+                    }                    
+                    else if ( right_idx < right_len )
+                    {
+                         const int prev_height_right = current_height_right;
+
+                         current_height_right = right_skyline[ right_idx + 1 ];
+
+                         if ( current_height_right > current_height_left )
+                         {
+                              res_skyline.push_back( right_skyline[ right_idx ] );                                                  
+                              res_skyline.push_back( current_height_right );
+
+                         }
+
+                         if ( prev_height_right > current_height_left &&
+                              current_height_left != 0 && current_height_right == 0 )
+                         {
+                              res_skyline.push_back( right_skyline[ right_idx ] );                                                  
+                              res_skyline.push_back( current_height_left );
+                         }
+
+                         if ( current_height_right == 0 && current_height_left == 0 )
+                         {
+                              res_skyline.push_back( right_skyline[ right_idx ] );                                                  
+                              res_skyline.push_back( 0 );
+                         }
+
+                         right_idx += 2;
+                         if ( right_idx < right_len )
+                         {
+                              current_right = right_skyline[ right_idx ];
+                         }
+                    }
+
+                    continue;
+                    ////////
+                    if ( left_skyline[ left_idx ] > right_skyline[ right_idx ] )
+                    {    
+                         const int prev_height_left = current_height_left;
+
+                         current_height_left = left_skyline[ left_idx + 1 ];
+
+                         if ( current_height_left > current_height_right )
+                         {
+                              res_skyline.push_back( left_skyline[ left_idx ] );                                                  
+                              res_skyline.push_back( current_height_left );
+
+                         }
+
+                         if ( prev_height_left > current_height_right && current_height_left == 0 )
+                         {
+                              res_skyline.push_back( left_skyline[ left_idx ] );                                                  
+                              res_skyline.push_back( current_height_right );
+                         }
+
+                         if ( current_height_right == current_height_left == 0 )
+                         {
+                              res_skyline.push_back( left_skyline[ left_idx ] );                                                  
+                              res_skyline.push_back( 0 );
+                         }
+
+                         left_idx += 2;
+                    }
+                    else
+                    {
+                         const int prev_height_right = current_height_right;
+                         
+                         current_height_right = right_skyline[ right_idx + 1 ];
+
+                         if ( current_height_right > current_height_left )
+                         {
+                              res_skyline.push_back( right_skyline[ right_idx ] );                                                  
+                              res_skyline.push_back( current_height_right );
+
+                         }
+
+                         if ( prev_height_right > current_height_left && current_height_right == 0 )
+                         {
+                              res_skyline.push_back( right_skyline[ right_idx ] );                                                  
+                              res_skyline.push_back( current_height_left );
+                         }
+
+                         if ( current_height_right == current_height_left == 0 )
+                         {
+                              res_skyline.push_back( right_skyline[ right_idx ] );                                                  
+                              res_skyline.push_back( 0 );
+                         }
+
+                         right_idx += 2;
+                    }
+               }
           }
      }
      
